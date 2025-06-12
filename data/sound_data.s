c1f7922a7798de5738d521479db74e51e5e3a108
eddie <grgnathan88@gmail.com>
2025-06-11 20:02:46 -0700

    Revert "remove mega cries and bard phonemes"
    This reverts commit a7ed8554cc1b23d9776393b889607c2fcbbca7dc.

--- data/sound_data.s
+++ data/sound_data.s
@@ -3,7 +3,6 @@
 	.include "asm/macros/m4a.inc"
 	.include "asm/macros/music_voice.inc"
 	.include "include/config/pokemon.h"
-	.include "include/config.h"
 	.include "sound/voice_groups.inc"
 	.include "sound/keysplit_tables.inc"
 	.include "sound/programmable_wave_data.inc"
